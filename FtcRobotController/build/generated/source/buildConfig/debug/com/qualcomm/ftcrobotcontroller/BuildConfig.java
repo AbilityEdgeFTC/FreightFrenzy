ce2d31698b3d7e33f2fa7f6f7143c4f03400f473
Elior Yousefi <elior@abilityedge.co.il>
2021-09-26 13:47:45 +0300

    .

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -10,5 +10,5 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 42;
   public static final String VERSION_NAME = "7.0";
   // Fields from default config.
-  public static final String APP_BUILD_TIME = "2021-09-26T13:46:29.418+0300";
+  public static final String APP_BUILD_TIME = "2021-09-26T13:47:07.216+0300";
 }
