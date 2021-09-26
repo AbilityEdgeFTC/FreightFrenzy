68f7ec7031e48b2aa194aa48a703a669d3c6ed94
Elior Yousefi <elior@abilityedge.co.il>
2021-09-26 13:32:03 +0300

    init

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -1,22 +1,16 @@
 /* Copyright (c) 2014, 2015 Qualcomm Technologies Inc
-
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without modification,
 are permitted (subject to the limitations in the disclaimer below) provided that
 the following conditions are met:
-
 Redistributions of source code must retain the above copyright notice, this list
 of conditions and the following disclaimer.
-
 Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
-
 Neither the name of Qualcomm Technologies Inc nor the names of its contributors
 may be used to endorse or promote products derived from this software without
 specific prior written permission.
-
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
 LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -61,6 +55,7 @@ import android.widget.LinearLayout.LayoutParams;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.google.blocks.ftcrobotcontroller.ProgrammingWebHandlers;
 import com.google.blocks.ftcrobotcontroller.runtime.BlocksOpMode;
 import com.qualcomm.ftccommon.ClassManagerFactory;
@@ -135,7 +130,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 @SuppressWarnings("WeakerAccess")
 public class FtcRobotControllerActivity extends Activity
-  {
+{
   public static final String TAG = "RCActivity";
   public String getTag() { return TAG; }
 
@@ -331,8 +326,9 @@ public class FtcRobotControllerActivity extends Activity
           }
         });
         popupMenu.inflate(R.menu.ftc_robot_controller);
+        FtcDashboard.populateMenu(popupMenu.getMenu());
         AnnotatedHooksClassFilter.getInstance().callOnCreateMenuMethods(
-            FtcRobotControllerActivity.this, popupMenu.getMenu());
+                FtcRobotControllerActivity.this, popupMenu.getMenu());
         popupMenu.show();
       }
     });
@@ -410,6 +406,8 @@ public class FtcRobotControllerActivity extends Activity
     checkPreferredChannel();
 
     AnnotatedHooksClassFilter.getInstance().callOnCreateMethods(this);
+
+    FtcDashboard.start();
   }
 
   protected UpdateUI createUpdateUI() {
@@ -483,6 +481,8 @@ public class FtcRobotControllerActivity extends Activity
     RobotLog.cancelWriteLogcatToDisk();
 
     AnnotatedHooksClassFilter.getInstance().callOnDestroyMethods(this);
+
+    FtcDashboard.stop();
   }
 
   protected void bindToService() {
@@ -538,6 +538,7 @@ public class FtcRobotControllerActivity extends Activity
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.ftc_robot_controller, menu);
+    FtcDashboard.populateMenu(menu);
     AnnotatedHooksClassFilter.getInstance().callOnCreateMenuMethods(this, menu);
     return true;
   }
@@ -592,7 +593,7 @@ public class FtcRobotControllerActivity extends Activity
       startActivityForResult(intentConfigure, RequestCode.CONFIGURE_ROBOT_CONTROLLER.ordinal());
     }
     else if (id == R.id.action_settings) {
-	  // historical: this once erroneously used FTC_CONFIGURE_REQUEST_CODE_ROBOT_CONTROLLER
+      // historical: this once erroneously used FTC_CONFIGURE_REQUEST_CODE_ROBOT_CONTROLLER
       Intent settingsIntent = new Intent(AppUtil.getDefContext(), FtcRobotControllerSettingsActivity.class);
       startActivityForResult(settingsIntent, RequestCode.SETTINGS_ROBOT_CONTROLLER.ordinal());
       return true;
@@ -628,7 +629,7 @@ public class FtcRobotControllerActivity extends Activity
       return true;
     }
 
-   return super.onOptionsItemSelected(item);
+    return super.onOptionsItemSelected(item);
   }
 
   @Override
@@ -706,7 +707,9 @@ public class FtcRobotControllerActivity extends Activity
     });
 
     AnnotatedHooksClassFilter.getInstance().callWebHandlerRegistrarMethods(this,
-        service.getWebServer().getWebHandlerManager());
+            service.getWebServer().getWebHandlerManager());
+
+    FtcDashboard.attachWebServer(service.getWebServer());
   }
 
   private void updateUIAndRequestRobotSetup() {
@@ -715,12 +718,12 @@ public class FtcRobotControllerActivity extends Activity
       callback.updateRobotStatus(controllerService.getRobotStatus());
       // Only show this first-time toast on headless systems: what we have now on non-headless suffices
       requestRobotSetup(LynxConstants.isRevControlHub()
-        ? new Runnable() {
-            @Override public void run() {
-              showRestartRobotCompleteToast(R.string.toastRobotSetupComplete);
-            }
-          }
-        : null);
+              ? new Runnable() {
+        @Override public void run() {
+          showRestartRobotCompleteToast(R.string.toastRobotSetupComplete);
+        }
+      }
+              : null);
     }
   }
 
@@ -740,6 +743,7 @@ public class FtcRobotControllerActivity extends Activity
       } catch (FileNotFoundException | XmlPullParserException e1) {
         RobotLog.ee(TAG, e1, "Failed to fall back on noConfig");
       }
+      FtcDashboard.attachEventLoop(eventLoop);
     }
 
     OpModeRegister userOpModeRegister = createOpModeRegister();
@@ -772,8 +776,8 @@ public class FtcRobotControllerActivity extends Activity
     requestRobotSetup(new Runnable() {
       @Override public void run() {
         showRestartRobotCompleteToast(R.string.toastRestartRobotComplete);
-        }
-      });
+      }
+    });
   }
 
   private void showRestartRobotCompleteToast(@StringRes int resid) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GreenLanternPipeline.java
@@ -0,0 +1,140 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public class GreenLanternPipeline extends OpenCvPipeline
+{
+    // we want to use webcam for our source.
+    OpenCvWebcam webcam;
+
+    // HSV low and high values for our team shipping element.
+    Scalar lowValues = new Scalar(86, 102, 11);
+    Scalar highValues = new Scalar(108, 203, 102);
+
+    // creating a mast with the same resolution of the webcam for the place to display the detected team shipping element
+    Mat mask = new Mat(1280,720,0);//
+
+    // creating 3 rectangles(sections) for checking the colors inside them.
+    final Rect LEFT_SEC = new Rect(
+            new Point(426.666667,720),//mask.cols()/7, mask.rows()/5 * 2
+            new Point(0,0));//mask.cols()/7 * 2, mask.rows()/5 * 4)
+
+
+    final Rect CENTER_SEC = new Rect(
+            new Point(853.333334,720),
+            new Point(426.666667,0));
+
+    final Rect RIGHT_SEC = new Rect(
+            new Point(1280,720 ),
+            new Point(853.333334,0 ));
+
+    boolean DEBUG = true;
+
+    double threshold_percentage = 0.4; // 40% of the element color
+
+    // the list of locations that can be
+    public enum Location{
+        Left,
+        Center,
+        Right,
+        Not_Found
+    }
+
+    private Location location;
+
+    // color for the rectangles to show on the screen
+    Scalar colorBarcodeRect = new Scalar(0, 255, 0);
+
+    Telemetry telemetry;
+
+    @Override
+    public Mat processFrame(Mat input) {
+
+        Imgproc.cvtColor(input, mask, Imgproc.COLOR_RGB2HSV);
+
+        // turning all colors not between the low and high values to black and the rest white.
+        Core.inRange(mask, lowValues, highValues, mask);
+
+        // taking sections from the mask to another mat
+        Mat left = mask.submat(LEFT_SEC);
+        Mat center = mask.submat(CENTER_SEC);
+        Mat right = mask.submat(RIGHT_SEC);
+
+        // calculating the average amount of color in the SECTION.
+        double leftAvg = Core.sumElems(left).val[0] / LEFT_SEC.area() / 255;
+        double centerAvg = Core.sumElems(center).val[0] / CENTER_SEC.area() / 255;
+        double rightAvg = Core.sumElems(right).val[0] / RIGHT_SEC.area() / 255;
+
+        // we release the mats for use.
+        left.release();
+        center.release();
+        right.release();
+
+        // if debugging so we display more values in telemetry.
+        if(DEBUG){
+            telemetry.addLine("Left raw value: " + (int) Core.sumElems(left).val[0]);
+            telemetry.addLine("Center raw value: " + (int) Core.sumElems(center).val[0]);
+            telemetry.addLine("Right raw value: " + (int) Core.sumElems(right).val[0]);
+
+            telemetry.addLine("Left average percentage: " + (int) Math.round(leftAvg * 100) + "%");
+            telemetry.addLine("Center average percentage: " + (int) Math.round(centerAvg * 100) + "%");
+            telemetry.addLine("Right average percentage: " + (int) Math.round(rightAvg * 100) + "%");
+            telemetry.update();
+        }
+
+        // comparing the average color in the left/center/right rect to the threshold.
+        boolean barcodeLeft = leftAvg > threshold_percentage;
+        boolean barcodeCenter = centerAvg > threshold_percentage;
+        boolean barcodeRight = rightAvg > threshold_percentage;
+
+        //checking which barcode is found.
+        if(barcodeLeft && barcodeCenter && barcodeRight){
+            // NOT FOUND
+            location = Location.Not_Found;
+            telemetry.addData("Barcode Location:","Unknown Barcode.");
+        }else if(barcodeLeft){
+            location = Location.Left;
+            telemetry.addData("Barcode Location:","LEFT Barcode.");
+        }else if(barcodeCenter){
+            location = Location.Center;
+            telemetry.addData("Barcode Location:","CENTER Barcode.");
+        }else if(barcodeRight){
+            location = Location.Right;
+            telemetry.addData("Barcode Location:","RIGHT Barcode.");
+        }else{
+            // NOT FOUND
+            location = Location.Not_Found;
+            telemetry.addData("Barcode Location:","Unknown Barcode.");
+        }
+        telemetry.update();
+
+        Imgproc.cvtColor(mask, mask, Imgproc.COLOR_GRAY2RGB);
+
+        // creating 3 rectangles at the mask mat with the color @colorBarcodeRect@ and the rectangle points LEFT/CENTER/RIGHT.
+        Imgproc.rectangle(mask, LEFT_SEC, colorBarcodeRect);
+        Imgproc.rectangle(mask, CENTER_SEC, colorBarcodeRect);
+        Imgproc.rectangle(mask, RIGHT_SEC, colorBarcodeRect);
+
+        return mask;
+    }
+
+    // getting location of the team shipping element.
+    public Location getLocation() {
+        return location;
+    }
+}
