a95c3a702c661b766a1fa45bf9df530d5a762c60
Elior Yousefi <elior@abilityedge.co.il>
2021-12-21 00:12:31 +0200

    dleted un necessary files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MenuOpModeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MenuOpModeTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmode.MenuSwitch;
+package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
