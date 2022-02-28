3f07862706fa9289ced3bb5c16401dc4a47aafef
Elior Yousefi <elior@abilityedge.co.il>
2022-02-28 18:45:30 +0200

    t265

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestT265.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestT265.java
@@ -2,39 +2,33 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.canvas.Canvas;
-import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Transform2d;
 import com.arcrobotics.ftclib.geometry.Translation2d;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.spartronics4915.lib.T265Camera;
 
-@Config
-@Autonomous(name="Test T265", group="Iterative Opmode")
-public class TestCameraOpMode extends OpMode
+@TeleOp(name="Test T265", group="Iterative Opmode")
+public class TestT265 extends OpMode
 {
     // We treat this like a singleton because there should only ever be one object per camera
     private static T265Camera slamra = null;
 
-    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+    public static double LATERAL_DISTANCE = -5.71; // in; offset of the camera from the left or right of the middle of the teleop.
+    public static double FORWARD_OFFSET = 2.3062992126; // in; offset of the camera from the front or back of the middle of the teleop.
+    public static double DIRECTION = -90;
 
-    public static double xCordinate = 0.375;
-    public static double yCordinate = -0.375;
-    public static double angleCordinate = 0;
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
     @Override
     public void init() {
         if (slamra == null) {
-            //slamra = new T265Camera(new Transform2d(new Translation2d(7.38,10.63),Rotation2d.fromDegrees(0)), 0.1, hardwareMap.appContext);
-            slamra = new T265Camera(new Transform2d(new Translation2d(xCordinate,yCordinate),Rotation2d.fromDegrees(angleCordinate)), 0.1, hardwareMap.appContext);
-
+            Translation2d translation2d = new Translation2d(FORWARD_OFFSET / 39.37,LATERAL_DISTANCE / 39.37);
+            Rotation2d rotation2d = new Rotation2d(Math.toRadians(DIRECTION));
+            slamra = new T265Camera(new Transform2d(translation2d, rotation2d), 0.1, hardwareMap.appContext);
         }
-
-        slamra.setPose(new Pose2d(0,0,Rotation2d.fromDegrees(0)));
     }
 
     @Override
@@ -48,7 +42,7 @@ public class TestCameraOpMode extends OpMode
 
     @Override
     public void loop() {
-        final double robotRadius = 6.79; // inches
+        final int robotRadius = 10; // inches
 
         TelemetryPacket packet = new TelemetryPacket();
         Canvas field = packet.fieldOverlay();
@@ -60,12 +54,6 @@ public class TestCameraOpMode extends OpMode
         Translation2d translation = new Translation2d(up.pose.getTranslation().getX() / 0.0254, up.pose.getTranslation().getY() / 0.0254);
         Rotation2d rotation = up.pose.getRotation();
 
-        telemetry.addData("X: ", translation.getX());
-        telemetry.addData("Y: ", translation.getX());
-        telemetry.addData("ROTATION: ", up.pose.getRotation());
-        telemetry.addData("CONFIDENCE: ", up.confidence);
-        telemetry.update();
-
         field.strokeCircle(translation.getX(), translation.getY(), robotRadius);
         double arrowX = rotation.getCos() * robotRadius, arrowY = rotation.getSin() * robotRadius;
         double x1 = translation.getX() + arrowX  / 2, y1 = translation.getY() + arrowY / 2;
@@ -73,6 +61,11 @@ public class TestCameraOpMode extends OpMode
         field.strokeLine(x1, y1, x2, y2);
 
         dashboard.sendTelemetryPacket(packet);
+
+        telemetry.addData("Pose X:", translation.getX());
+        telemetry.addData("Pose Y:", translation.getY());
+        telemetry.addData("Pose HEADING:", rotation.getRadians());
+        telemetry.update();
     }
 
     @Override
