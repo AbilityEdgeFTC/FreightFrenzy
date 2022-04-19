c8fed869030c80f6256d2295ff5c506c7bdfcc47
noampima <104015037+noampima@users.noreply.github.com>
2022-04-20 01:21:22 +0300

    Added Red Convectors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonmous/Converter/RedPose2DConverter.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonmous.Converter;
+
+public class RedPose2DConverter {
+
+    public static double RedstartPoseRightX;
+    public static double RedstartPoseRightY;
+    public static double RedstartPoseRightH;
+    public static double RedstartPoseLeftX;
+    public static double RedstartPoseLeftH;
+    public static double RedstartPoseLeftY;
+    public static double RedposeEntranceX;
+    public static double RedposeEntranceY;
+    public static double RedposeEntranceH;
+    public static double RedposeCollectX;
+    public static double RedposeCollectY;
+    public static double RedposeCollectH;
+    public static double RedposeHelpX;
+    public static double RedposeHelpY;
+    static double RedposeHelpH;
+
+}
+
