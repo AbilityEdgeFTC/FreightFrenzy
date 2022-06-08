0ee186c409c96fd2154bd0342b16f33deea369c4
EitanKravets <eitankr200624@gmail.com>
2022-06-08 19:41:20 +0300

    cap test
    cap test first try

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Cap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Cap.java
@@ -11,32 +11,47 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
 
 @Config
 public class Cap {
 
     CRServo spin;
     Servo upDown;
-    Servo claw;
+    Servo hook;
     public static double openingPosition = 0, closingPosition = 0;
     public static double holdUp = 0, holDown = 0, placeCap = 0;
     Gamepad gamepad;
+    cGamepad cGamepad;
+
+    boolean isToggle = false;
 
     // 2 constructors for 2 options, construct the carousel with and without telementry.
     /** THE CONSTRUCTOR GET THE MOTOR TO SPIN, POWER FOR THAT MOTOR, AND HARDWAREMAP.  */
     public Cap(HardwareMap hardwareMap, Gamepad gamepad) {
         this.spin = hardwareMap.get(CRServo.class, "crSpin");
         this.upDown = hardwareMap.get(Servo.class, "sC");
-        this.claw = hardwareMap.get(Servo.class, "sC");
+        this.hook = hardwareMap.get(Servo.class, "sC");
         this.gamepad = gamepad;
+        this.cGamepad = new cGamepad(gamepad);
     }
 
     public void update()
     {
-
+        spin.setPower(gamepad.left_stick_x);
+        if (isToggle == false)
+        {
+            upDown.setPosition(gamepad.left_stick_y);
+            hook.setPosition(gamepad.left_stick_y);
+        }
+        else
+        {
+            upDown.setPosition(gamepad.left_stick_y);
+            hook.setPosition(gamepad.right_stick_y);
+        }
+        if (cGamepad.AOnce())
+            isToggle = !isToggle;
     }
+
 //
 //    // move dip servo to intake position.
 //    public void closeCover() {
