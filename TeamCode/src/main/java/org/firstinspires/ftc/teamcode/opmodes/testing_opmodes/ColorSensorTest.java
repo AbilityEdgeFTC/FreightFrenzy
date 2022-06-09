35155845efb1763185f670a3e25cf004ce162143
Elior Yousefi <elior@abilityedge.co.il>
2022-06-09 23:37:07 +0300

    d

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing_opmodes/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testing_opmodes/ColorSensorTest.java
@@ -34,8 +34,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.robot.subsystems.SensorColor;
-
 
 @TeleOp(name="ColorSensorTest")
 @Disabled
